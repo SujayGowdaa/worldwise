f37edfcb576aa67ab032f5f0924300919b83bf9d
SUJAY GOWDA <sujay.gowda@i4sight.net>
2023-12-10 11:20:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
